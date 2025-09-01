212bfb90fd36805db67bc08307b3031c9e723734
troy8865 <et1903y@hotmail.com>
2025-09-01 22:28:53 +0300

    Update m3u.py

--- m3u.py
+++ m3u.py
@@ -33,6 +33,8 @@ def dlhd():
     HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
     }
+    # URL'lerin başına eklenecek ve linklerin çalışmasını sağlayacak olan proxy ön eki
+    proxy_prefix = "https://pulutotv-alsancak.hf.space/proxy/m3u?url="
 
     # ========== FUNZIONI DI SUPPORTO ==========
     def clean_category_name(name):
